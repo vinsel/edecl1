2264046d0717583712acfcae04c7152b034bcf49
Vinsel <vinselrogue@gmail.com>
2020-01-15 16:28:41 +0100

    modificado cpp cancion

--- holamundoc++.cpp
+++ holamundoc++.cpp
@@ -6,6 +6,11 @@ int main() {
 
     cout << "Hola Manolo, vendo droga" << endl;
     cout << "I like trains" << endl;
+    cout << "I like big" << endl;
+    cout << "butts and i cannot lie!" << endl;
+    cout << "they call me cuban pete" << endl;
+    cout << "im the king of maracas papi" << endl;
+    cout << "when i take the maracas i go chik chiki boom" << endl;
 
     return 0;
 
